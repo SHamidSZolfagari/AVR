1119ea942b6f5b75f03e4d3487b28ef1cbd8475e
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2009-11-16 02:03:48 -0300

    Agregado un nuevo proyecto: cartel

---
+++ cartel/cartel.c
@@ -0,0 +1,44 @@
+#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+#include "font5x7.h"
+
+#define DS	_BV(PINC0)
+#define SH_CP	_BV(PINC1)
+#define ST_CP	_BV(PINC2)
+
+void main(void)
+{
+	DDRC = 0x07;
+	DDRB = 0x7F;
+	
+	int x,y,d;
+	
+	char msg[] = "Linux :)";
+
+	while(1) {
+		for (x = 0; x < 40; x++) {
+			if (x == 0) {
+				y = -1;
+				PORTC ^= DS;
+				PORTC ^= SH_CP;
+				PORTC ^= ST_CP;
+				PORTC ^= DS;
+			} else {
+				PORTC ^= SH_CP;
+				PORTC ^= ST_CP;
+			}
+			PORTC ^= SH_CP;
+			PORTC ^= ST_CP;
+			if (x % 5 == 0) {
+				y++;
+			}
+			PORTB = ~(pgm_read_byte(&(Font5x7[(msg[y] - 0x20)*5 + x % 5])));
+			/* PORTB = ~0x7F; */
+			_delay_ms(1);
+			PORTB = 0x7F;
+		}
+	}
+}
