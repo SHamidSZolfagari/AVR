d44d4c25527fc6eb25473d64101c9865b1d8177b
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2009-11-24 02:08:10 -0300

    [cuenta] Varias mejoras
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- cuenta/cuenta.c
+++ cuenta/cuenta.c
@@ -1,20 +1,32 @@
 #define F_CPU 1000000UL
 
-#define MEDICIONES 6
+#define MAX_MEDICIONES 17
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
 
-static void uart_init(void);
 static int uart_getchar(FILE *stream);
 static int uart_putchar(char dato, FILE *stream);
 
-static unsigned int valores[MEDICIONES];
+static unsigned int valores[MAX_MEDICIONES];
 static int i = 0;
-
+static int m = 6;
 
 static FILE rs232 = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
+
+ISR (INT0_vect)
+{
+	if (i == 0)
+		TCCR1B = _BV(CS12) | _BV(CS10); /* prescaler de 1024 */
+	else if (i == m) {
+		TCCR1B = 0;
+		TCNT1 = 0;
+		i = 0;
+		return;
+	}
+	valores[i++] = TCNT1;
+}
 
 static void uart_init(void)
 {
@@ -25,39 +37,82 @@ static void uart_init(void)
 
 static int uart_getchar(FILE *stream)
 {
-	loop_until_bit_is_set(UCSRA, TXC);
-	return UDR;
+	char valor;
+	loop_until_bit_is_set(UCSRA, RXC);
+	if (UCSRA & (_BV(FE) | _BV(DOR)))
+		return _FDEV_ERR;
+	else
+		if ((valor = UDR) == '\r')
+			return '\n';
+		else
+			return valor;
 }
 
 static int uart_putchar(char dato, FILE *stream)
 {
+	if (dato == '\n')
+		uart_putchar('\r', stdout);
 	loop_until_bit_is_set(UCSRA, UDRE);
 	UDR = dato;
 	return 0;
 }
 
-ISR (INT0_vect)
+static void volcar(void)
 {
-	if (i == 0)
-		TCCR1B = _BV(CS12) | _BV(CS10); /* prescaler de 64 */
-	else if (i == MEDICIONES) {
-		int n;
-		TCCR1B = 0;
-		TCNT1 = 0;
-		i = 0;
-		for (n = 0; n < MEDICIONES; n++) {
-			printf("%u\r", valores[n]);
-		}
-		return;
+	int n;
+	printf("\n");
+	for (n = 0; n < m; n++) {
+		printf("%u\n", valores[n]);
 	}
-	valores[i++] = TCNT1;
 }
 
+static int menu(void)
+{
+	printf("\n");
+	printf("[r] Resetear\n");
+	printf("[v] Ver valores\n");
+	printf("[c] Cantidad de mediciones (%d)\n", m);
+	printf("\n? ");
+	
+	return getchar();
+}
+
+static void configuracion(void)
+{
+	int x;
+	char buf[4];
+	printf("\n(c) ? ");
+	fgets(buf, sizeof(buf), stdin);
+	sscanf(buf, "%d", &x);
+	if (x > 0 && x < 18) {
+		m = x;
+		TCNT1 = 0;
+		i = 0;
+	}
+	else
+		printf("\nERROR: %d, fuera de rango [1, 17]\n", x);
+}
 
 void main(void)
 {
 	uart_init();
 	GICR = _BV(INT0);
 	sei();
-	while(1);
+	while(1) {
+		switch(menu()) {
+			case 'r':
+				TCCR1B = 0;
+				TCNT1 = 0;
+				i = 0;
+				break;
+			case 'v':
+				volcar();
+				break;
+			case 'c':
+				configuracion();
+				break;
+			default:
+				printf("ERROR: opcion invalida\n");
+		}
+	}
 }
