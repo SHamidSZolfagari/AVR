371591b5d5c808876b45b324e461e3a147eb6a4f
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2009-11-25 00:24:16 -0300

    [cuenta] varias mejoras y posibilidades de depuracion
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- cuenta/cuenta.c
+++ cuenta/cuenta.c
@@ -1,6 +1,6 @@
 #define F_CPU 1000000UL
 
-#define MAX_MEDICIONES 17
+#define MAX_MEDICIONES 17 /* 17 mediciones sin sobrepasar el timer a pulsos de 1Hz */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -9,20 +9,23 @@
 static int uart_getchar(FILE *stream);
 static int uart_putchar(char dato, FILE *stream);
 
-static unsigned int valores[MAX_MEDICIONES];
+static unsigned int valores[MAX_MEDICIONES] = {0};
 static int i = 0;
 static int m = 6;
+static int d = 0;
 
 static FILE rs232 = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
 ISR (INT0_vect)
 {
-	if (i == 0)
+	if (d == 1)
+		printf("%d\n", i);
+	if (i == 0) {
 		TCCR1B = _BV(CS12) | _BV(CS10); /* prescaler de 1024 */
+	}
 	else if (i == m) {
-		TCCR1B = 0;
-		TCNT1 = 0;
-		i = 0;
+		TCCR1B = 0; /* detiene el timer */
+		TCNT1 = 0; /* timer a cero */
 		return;
 	}
 	valores[i++] = TCNT1;
@@ -30,16 +33,16 @@ ISR (INT0_vect)
 
 static void uart_init(void)
 {
-	UCSRB = _BV(TXEN) | _BV(RXEN);
-	UBRRL = 25;
+	UCSRB = _BV(TXEN) | _BV(RXEN); /* TX y RX habilitados */
+	UBRRL = 25; /* 2400 baudios a 1MHz */
 	stdin = stdout = stderr = &rs232;
 }
 
 static int uart_getchar(FILE *stream)
 {
 	char valor;
-	loop_until_bit_is_set(UCSRA, RXC);
-	if (UCSRA & (_BV(FE) | _BV(DOR)))
+	loop_until_bit_is_set(UCSRA, RXC); /* espero datos */
+	if (UCSRA & (_BV(FE) | _BV(DOR))) /* datos OK ? */
 		return _FDEV_ERR;
 	else
 		if ((valor = UDR) == '\r')
@@ -52,7 +55,7 @@ static int uart_putchar(char dato, FILE *stream)
 {
 	if (dato == '\n')
 		uart_putchar('\r', stdout);
-	loop_until_bit_is_set(UCSRA, UDRE);
+	loop_until_bit_is_set(UCSRA, UDRE); /* espero que este libre el TX */
 	UDR = dato;
 	return 0;
 }
@@ -71,6 +74,8 @@ static int menu(void)
 	printf("\n");
 	printf("[r] Resetear\n");
 	printf("[v] Ver valores\n");
+	printf("[m] Mostrar timer\n");
+	printf("[d] Mostrar extint (%d)\n", d);
 	printf("[c] Cantidad de mediciones (%d)\n", m);
 	printf("\n? ");
 	
@@ -96,13 +101,17 @@ static void configuracion(void)
 void main(void)
 {
 	uart_init();
-	GICR = _BV(INT0);
+	PORTD = _BV(PD2); /* resistor de pull-up */
+	MCUCR = _BV(ISC01); /* int0 por flanco H->L */
+	GICR = _BV(INT0); /* int0 habilitada */
 	sei();
 	while(1) {
 		switch(menu()) {
 			case 'r':
-				TCCR1B = 0;
-				TCNT1 = 0;
+				TCCR1B = 0; /* detengo el timer */
+				TCNT1 = 0; /* timer a cero */
+				for (i = 0; i < MAX_MEDICIONES; i++)
+					valores[i] = 0;
 				i = 0;
 				break;
 			case 'v':
@@ -111,8 +120,14 @@ void main(void)
 			case 'c':
 				configuracion();
 				break;
+			case 'm':
+				printf("\n%u\n", TCNT1);
+				break;
+			case 'd':
+				d = !d;
+				break;
 			default:
-				printf("ERROR: opcion invalida\n");
+				printf("\n ERROR: opcion invalida\n");
 		}
 	}
 }
