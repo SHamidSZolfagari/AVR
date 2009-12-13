a0cb4bc027008e49dd02f353d762e41af9c96f0d
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2009-12-13 18:08:57 -0300

    [test232] removida una variable sin uso, y flags por default

--- test232/test232.c
+++ test232/test232.c
@@ -1,7 +1,6 @@
 #define F_CPU 7372800UL
 
 #include <avr/io.h>
-#include <util/delay.h>
 
 void enviar_data(unsigned char dato)
 {
@@ -21,12 +20,9 @@ unsigned char recibir_data(void)
 
 void main (void)
 {
-	unsigned char data=0;
 	UBRRL = 47; /* 9600 a 7.3728 MHz */
 	UCSRB = (1 << RXEN) | (1 << TXEN);
-	UCSRC = (1 << URSEL) | (1 << UCSZ1) | (1 << UCSZ0);
 	while(1) {
-		data = recibir_data();
-		enviar_data(data);
+		enviar_data(recibir_data());
 	}
 }
